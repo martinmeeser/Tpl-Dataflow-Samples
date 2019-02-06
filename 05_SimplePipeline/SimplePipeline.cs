9851a2c15ab6034ec3d095de6a82946c2c0fb435
Martin Meeser <martin.meeser@softwareing.de>
2019-02-06 07:14:14 +0100

    Finishing the pipe-line example

--- 05_SimplePipeline/SimplePipeline.cs
+++ 05_SimplePipeline/SimplePipeline.cs
@@ -1,11 +1,12 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 public class SimplePipeline
 {
 
-    public static async Task CreateSampleFile(string path, int totalCount, int countPerLine)
+    public static async Task CreateSampleCsvFile(string path, int totalCount, int countPerLine)
     {
         int lines = totalCount / countPerLine;
         Random random = new Random();
@@ -34,7 +35,77 @@ public class SimplePipeline
             }
         }
 
+        streamWriter.Close();
     }
+
+
+    public static async Task ExecutePipelineSample(string path)
+    {
+        // blocks
+        var splitStrings = new TransformManyBlock<string, string>(
+            line => line.Split(';')
+        , new ExecutionDataflowBlockOptions { });
+
+        var parseStrings = new TransformBlock<string, double?>(s =>
+        {
+            if (!string.IsNullOrEmpty(s))
+            {
+                if (Double.TryParse(s, out double result))
+                {
+                    return result;
+                }
+                Console.WriteLine($"Parse Error: {s}");
+            }
+            return null;
+        }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 4 });
+
+        double? min = null;
+        double? max = null;
+        ulong count = 0;
+
+        var minMax = new ActionBlock<double?>(d =>
+        {
+            if (d.HasValue)
+            {
+                if (!min.HasValue) { min = d.Value; }
+                if (!max.HasValue) { max = d.Value; }
+
+                if (min > d.Value) { min = d.Value; }
+                if (max < d.Value) { max = d.Value; }
+
+                count++;
+            }
+        }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 4 });
+
+        // links
+        splitStrings.LinkTo(parseStrings, new DataflowLinkOptions { PropagateCompletion = true });
+        parseStrings.LinkTo(minMax, new DataflowLinkOptions { PropagateCompletion = true });
+
+        try
+        {
+            StreamReader streamReader = new StreamReader(path);
+            {
+                string currLine;
+                Console.WriteLine($"Starting: {DateTime.Now}");
+                while ((currLine = streamReader.ReadLine()) != null)
+                {
+                    await splitStrings.SendAsync(currLine);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        splitStrings.Complete();
+        await Task.WhenAll(splitStrings.Completion, parseStrings.Completion, minMax.Completion);
+
+        Console.WriteLine($"Stopped: {DateTime.Now}");
+        Console.WriteLine($"Computed {count} values, min is {min}, max is {max}");
+
+    }
+
 
     private static double NextDoubleInMinMaxRange(Random random)
     {
